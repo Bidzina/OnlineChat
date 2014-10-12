a415153fab34ad505ae22748b6398cd443f10f22
bidzina <b_aivazashvili@cu.edu.ge>
2014-10-12 16:47:28 +0400

    Day 1

---
+++ Login.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+	<title>Online Chat</title>
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</head>
+<body>
+<div>
+<form class="form-group">
+<input type="text" class="form-control"  name="firstname" placeholder="Enter your Firstname" />
+<input type="text" class="form-control" id="exampleInputPassword1" name="Lastname" placeholder="Enter your Lastname" />
+<input type="email" class="form-control" name="email" Placeholder="Enter your Email" />
+<input type="password" class="form-control" name="password" placeholder="Enter your Password" />
+<input type="password" class="form-control" name="re-password" placeholder="Re-enter your password" />
+<input type="date" class="form-control" name="bday" />
+<input type="radio" class="sex" name="sex" value="male">Male<br>
+<input type="radio" class="sex" name="sex" value="female">Female<br>
+<input type="submit" class="btn btn-success" name="submit" value="Sign Up">
+
+</form>
+</div>
+
+</body>
+</html>
